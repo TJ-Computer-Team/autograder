ab0a6576cb107ea6f86d60444ac9f3df366efa8e
Gabriel Xu <105399165+Gabriel-Xu@users.noreply.github.com>
2024-08-15 10:19:14 -0400

    Update runTests.js

--- routes/runTests.js
+++ routes/runTests.js
@@ -1,9 +1,6 @@
-const {testSql, grabProblem, insertSubmission, grabStatus, grabTests, updateTestSol, grabChecker} = require("./sql");
-const execSync = require('child_process').execSync;
+const {grabProblem, insertSubmission, grabStatus} = require("./sql");
 const axios = require('axios');
-const fs = require('fs');
 const querystring = require('querystring');
-
 
 let tasks = [], tasksS = [];
 let running = false;
@@ -11,50 +8,38 @@ let queuePaused = false;
 function queue(pid, sid) {
 	tasks.push(pid);
 	tasksS.push(sid);
-	//console.log(pid, sid);
 	if (!running && !queuePaused) {
 		run();
 		running = true;
 	}
 }
 async function run() {
-        running = true;
+    running = true;
 	if (tasks.length == 0) {
-                running = false;
-                return;
-        } else {
+		running = false;
+		return;
+	} else {
         let task = tasks.shift();
         let sub = tasksS.shift();
-        console.log("running task:", task, sub);
+        console.log("Running task:", task, sub);
         let res = await grabProblem(task);
         let checkid = res.checkid;
         let tl = res.tl;
         let ml = res.ml;
         res = await grabStatus(sub);
         let userCode = res.code;
-
         let language = res.language;
-
         let output = undefined, fverdict = "ER", runtime = -1, memory = 100;
 
-        //console.log("\n\n\n-------------------");
-
-	// DOESNT WORK await sleep(1000);
-
-        await axios.post('http://10.150.0.3:8080/run', querystring.stringify({lang: language, problemid: String(task), code: userCode}))
-        .then(res => {
-		return res['data']
+        await axios.post('http://10.150.0.3:8080/run', querystring.stringify({lang: language, problemid: String(task), code: userCode})
+		).then(res => {
+			return res['data']
         }).then(res =>{
-		insertSubmission(sub, res.verdict, res.tl, memory, res.output);
-		//setTimeout(run(), 1000);
-		if (!queuePaused) run();
-
-	}).catch((error) => {
-		//console.log("ERROR WITH GRADING SERVER");
-                //console.log(error);
-		insertSubmission(sub, "ERROR", res.tl, memory, "grading server error:\n" + error);
-		//setTimeout(run(), 1000);
-		if (!queuePaused) run();
+			insertSubmission(sub, res.verdict, res.tl, memory, res.output);
+			if (!queuePaused) run();
+		}).catch((error) => {
+			insertSubmission(sub, "ERROR", res.tl, memory, "Grading server error:\n" + error);
+			if (!queuePaused) run();
         });
 	}
 }
@@ -86,7 +71,7 @@ module.exports = {
 			if (tasksS[i] == sid) {
 				tasksS.splice(i, 1);
 				tasks.splice(i, 1);
-				insertSubmission(sid, "SKIPPED", 0, 0, "Your submission was maunally skipped by an admin.");
+				insertSubmission(sid, "SKIPPED", 0, 0, "Your submission was manually skipped by an admin");
 				break;
 			}
 		}
