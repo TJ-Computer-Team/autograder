540395c348d54586716147ded98f6feae07f9b68
Gabriel Xu <105399165+Gabriel-Xu@users.noreply.github.com>
2024-11-22 19:47:57 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -5,17 +5,17 @@ const {pl} = require('./routes/sql');
 async function check(user_data, req, res) {
     try {
         // communicate with the database
-        await pl.connect();
+        const cl = await pl.connect();
 
         // fetch our user from the database
         // prompt other behavior if they're new
-        let results = await pl.query("SELECT * FROM users WHERE id = ($1)", [user_data.id]);
+        let results = await cl.query("SELECT * FROM users WHERE id = ($1)", [user_data.id]);
         if (results.rows.length == 0) { // first time user
-            await pl.end();
+            cl.release();
             req.session.user_data = user_data;
             res.redirect("/start");
         } else {
-            await pl.end();
+            cl.release();
             req.session.name = results.rows[0].display_name;
             req.session.username = results.rows[0].username;
             req.session.userid = results.rows[0].id;
@@ -42,9 +42,9 @@ async function check(user_data, req, res) {
 // file in the data into the database for a first-time user
 async function populate(user_data, req, res) { 
     try {
-        await pl.connect();
-        await pl.query("INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6)", [user_data.id, user_data.display_name, user_data.ion_username, false, user_data.email, user_data.pass]);
-        await pl.end();
+        const cl = await pl.connect();
+        await cl.query("INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6)", [user_data.id, user_data.display_name, user_data.ion_username, false, user_data.email, user_data.pass]);
+        cl.release();
         await check(user_data, req, res);
     } catch (error) {
         console.log(error);
