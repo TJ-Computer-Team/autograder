f0dfe5104eb6ce19f355b2e59d617ecfed67b7a8
Gabriel Xu <105399165+Gabriel-Xu@users.noreply.github.com>
2024-11-22 19:42:55 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,29 +1,21 @@
 // this file has functions to exectute the second half of the OAuth after the request to Ion API has been completed
 
 const Client = require('pg').Client;
-const pl = require(./
-let cl = undefined;
+const {pl} = require('./routes/sql');
 async function check(user_data, req, res) {
     try {
         // communicate with the database
-        // note: this is one of the instances that doesn't use the pool created in routes/sql.js (which might be bad)
-        cl = new Client({
-            user: "postgres",
-            password: process.env.PGPASSWORD,
-            port: 5432,
-            database: "autograder"
-        });
-        await cl.connect();
+        await pl.connect();
 
         // fetch our user from the database
         // prompt other behavior if they're new
-        let results = await cl.query("SELECT * FROM users WHERE id = ($1)", [user_data.id]);
+        let results = await pl.query("SELECT * FROM users WHERE id = ($1)", [user_data.id]);
         if (results.rows.length == 0) { // first time user
-            await cl.end();
+            await pl.end();
             req.session.user_data = user_data;
             res.redirect("/start");
         } else {
-            await cl.end();
+            await pl.end();
             req.session.name = results.rows[0].display_name;
             req.session.username = results.rows[0].username;
             req.session.userid = results.rows[0].id;
@@ -50,15 +42,9 @@ async function check(user_data, req, res) {
 // file in the data into the database for a first-time user
 async function populate(user_data, req, res) { 
     try {
-        cl = new Client({
-            user: "postgres",
-            password: process.env.PGPASSWORD,
-            port: 5432,
-            database: "autograder"
-        });
-        await cl.connect();
-        await cl.query("INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6)", [user_data.id, user_data.display_name, user_data.ion_username, false, user_data.email, user_data.pass]);
-        await cl.end();
+        await pl.connect();
+        await pl.query("INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6)", [user_data.id, user_data.display_name, user_data.ion_username, false, user_data.email, user_data.pass]);
+        await pl.end();
         await check(user_data, req, res);
     } catch (error) {
         console.log(error);
