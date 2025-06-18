d17f37c1a0bfe694eca7ff469bb9b2753718bf59
ilikecheezits <samuelzhang71@gmail.com>
2025-06-18 19:30:38 -0400

    Integrate ilikecheezits/TJCT changes: Add root app.js, oauth.js, profile.js, package.json and fix view paths

---
+++ app.js
@@ -0,0 +1,90 @@
+require('dotenv').config();
+const session = require('express-session');
+const express = require("express");
+const app = express();
+const port = 3000;
+const gradeRouter = require("./autograder/routes/grade");
+const adminRouter = require("./autograder/routes/admin");
+const {
+    populate
+} = require("./profile");
+const useragent = require("express-useragent");
+
+var favicon = require('serve-favicon');
+app.use(favicon("./autograder/public/images/Logo.ico"));
+
+app.use(express.static("./autograder/public"));
+app.use(express.urlencoded({
+    extended: false
+}));
+
+// For personal setup, create a .env file to store secret OAuth key
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use("/grade", gradeRouter);
+app.use("/admin", adminRouter);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/autograder/views');
+
+console.log('Views directory:', app.get('views'));
+
+// Home page
+app.get("/", async (req, res) => {
+    let source = req.headers['user-agent'];
+    let ua = await(useragent.parse(source));
+    if (ua.isMobile) {
+        // We don't support functionality on mobile
+        req.session.mobile = true;
+        res.status(200);
+        res.render("phone", {
+            loginurl: "/grade/authlogin"
+        });
+    } else {
+        req.session.mobile = false;
+        res.status(200).render("index", {
+            loginurl: "/grade/authlogin"
+        });
+    }
+});
+
+// Route to handle first time users
+app.get("/start", async (req, res) => {
+    if (req.session.user_data == undefined) {
+        res.redirect("/");
+    } else {
+        deviceClass = 'main'
+        if (req.session.mobile) {
+            deviceClass = 'phone';
+        }
+        res.render("firstTime", {
+            name: req.session.user_data.display_name,
+            username: req.session.user_data.ion_username,
+            device: deviceClass
+        });
+    }
+});
+
+// Post request after first time users receive their information
+app.post("/confirm", async (req, res) => {
+    user_data = req.session.user_data;
+    user_data.email = req.body.email;
+    user_data.pass = "1234"; //filler key
+    //user_data.pass = req.body.pass; //this feature is not being used anymore
+    try {
+        if (req.body.email.length > 100) {
+            res.send("Your input exceeds the limit");
+        } else {
+            await populate(user_data, req, res);
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+
+console.log("Application Started");
+app.listen(port); 
---
+++ oauth.js
@@ -0,0 +1,71 @@
+// This file handles part of the OAuth functionality
+
+const {
+    AuthorizationCode
+} = require('simple-oauth2');
+const axios = require('axios');
+const config = {
+    client: {
+        id: process.env.CLIENT_ID,
+        secret: process.env.CLIENT_SECRET
+    },
+    auth: {
+        tokenHost: 'https://ion.tjhsst.edu/oauth/',
+        authorizePath: 'https://ion.tjhsst.edu/oauth/authorize',
+        tokenPath: 'https://ion.tjhsst.edu/oauth/token/'
+    }
+};
+const client = new AuthorizationCode(config);
+async function getToken() {
+    const login_url = client.authorizeURL({
+        redirect_uri: process.env.CLIENT_REDIRECT_URI,
+        scope: ['read']
+    });
+    return login_url;
+}
+
+// Makes a call to the TJHSST Ion API with your information after the user gives perms to tjctgrader
+async function processFunction(CODE, req, res2) {
+    const tokenParams = {
+        code: CODE,
+        redirect_uri: process.env.CLIENT_REDIRECT_URI,
+        scope: ['read']
+    };
+    try {
+        let accessToken = await client.getToken(tokenParams);
+        let vals = undefined;
+        let content = { 'Authorization': 'Bearer ' + accessToken.token.access_token};
+        console.log("OAuth request content:", content);
+        await axios.get('https://ion.tjhsst.edu/api/profile?format=json', {
+                headers: content
+            }).then(res => {
+                let user_data = res.data;
+                req.session.id = user_data.id;
+                req.session.accessToken = accessToken;
+
+                // user_data contains information such as ion_id that we display
+                vals = {
+                    user_data: user_data,
+                    req: req,
+                    res: res2
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                res2.redirect("/");
+            });
+        return vals;
+    } catch (error) {
+        console.log('Access Token Error:', error.message);
+        return false;
+    }
+}
+
+module.exports = {
+    getToken: () => {
+        return getToken();
+    },
+    processFunction: (CODE, req, res) => {
+        return processFunction(CODE, req, res);
+    }
+} 
